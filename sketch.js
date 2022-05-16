7a0788810f4cd660b82c200f6d0e59cb04c27287
riti2009 <67743556+riti2009@users.noreply.github.com>
2022-05-16 15:53:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var box;
+var imagem;
+function preload(){
+imagem = loadImage('mario.png')
+}
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+box.addImage(imagem)
+}
+
+function draw() 
+{
+   background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
